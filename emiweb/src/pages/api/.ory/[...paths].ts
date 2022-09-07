2c5d35a1e299fbffc0cb32fec19f0da1c1497b9b
Sebastian Pietras <spietras1998@gmail.com>
2022-09-07 03:58:05 +0200

    Moved to using gateway only for API calls (#21)

--- emiweb/src/pages/api/.ory/[...paths].ts
+++ emiweb/src/pages/api/.ory/[...paths].ts
@@ -1,7 +1,7 @@
 import { config, createApiHandler } from "@ory/integrations/next-edge";
 
-const baseUrl = process.env.EMIWEB_AUTHGATE_URL || "http://localhost:20000";
-const apiUrl = `${baseUrl}/authe`;
+const baseUrl = process.env.EMIWEB_EMIGATE_URL || "http://localhost:20000";
+const apiUrl = `${baseUrl}/auth/authe`;
 
 export { config };
 
