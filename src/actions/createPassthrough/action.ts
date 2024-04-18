3cac9b8ce6762f26777a3f679e378d94ed12b069
Sebastian Pietras <me@spietras.dev>
2024-04-18 19:34:08 +0200

    Changed environment variables (#35)

--- src/actions/createPassthrough/action.ts
+++ src/actions/createPassthrough/action.ts
@@ -5,16 +5,19 @@ import { CreatePasthroughProps } from "./types";
 
 const errorMessage = "Creating passthrough failed.";
 
+const host = process.env.EMIWEB__EMISTREAM__SRT__HOST || "localhost";
+const port =
+  process.env.EMIWEB__EMISTREAM__SRT__PORT === undefined
+    ? 10000
+    : parseInt(process.env.EMIWEB__EMISTREAM__SRT__PORT, 10);
+
 export async function createPassthrough({ password }: CreatePasthroughProps) {
   try {
     const { data, error } = await emipass.POST("/stream", {
       body: {
         srt: {
-          host: process.env.EMIWEB__EMISTREAM__SRT__HOST || "localhost",
-          port:
-            process.env.EMIWEB__EMISTREAM__SRT__PORT === undefined
-              ? 10000
-              : parseInt(process.env.EMIWEB__EMISTREAM__SRT__PORT, 10),
+          host: host,
+          port: port,
           password: password,
         },
       },
--- src/api/emipass/client.ts
+++ src/api/emipass/client.ts
@@ -3,6 +3,8 @@ import "server-only";
 import createClient from "openapi-fetch";
 import type { paths } from "./types";
 
-export const emipass = createClient<paths>({
-  baseUrl: process.env.EMIWEB__EMIPASS__HTTP__URL || "http://localhost:11000",
-});
+const host = process.env.EMIWEB__EMIPASS__HTTP__HOST || "localhost";
+const port = process.env.EMIWEB__EMIPASS__HTTP__PORT || "11000";
+const url = `http://${host}:${port}`;
+
+export const emipass = createClient<paths>({ baseUrl: url });
