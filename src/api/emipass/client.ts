ec74e1d063378dfa0017c5f4a41ee9989618cff9
Sebastian Pietras <me@spietras.dev>
2024-04-29 13:47:12 +0200

    Improved URLs (#41)

--- src/api/emipass/client.ts
+++ src/api/emipass/client.ts
@@ -5,8 +5,15 @@ import type { paths } from "./types";
 
 const scheme = process.env.EMIWEB__EMIPASS__HTTP__SCHEME || "http";
 const host = process.env.EMIWEB__EMIPASS__HTTP__HOST || "localhost";
-const port = process.env.EMIWEB__EMIPASS__HTTP__PORT || "11000";
-const path = process.env.EMIWEB__EMIPASS__HTTP__PATH || "";
-const url = `${scheme}://${host}:${port}${path}`;
+const port =
+  process.env.EMIWEB__EMIPASS__HTTP__PORT === undefined
+    ? 11000
+    : process.env.EMIWEB__EMIPASS__HTTP__PORT;
+const path = (process.env.EMIWEB__EMIPASS__HTTP__PATH || "")
+  // Ensure path starts with a slash
+  .replace(/^(?!\/)(.*)$/, "/$1")
+  // Remove trailing slashes
+  .replace(/\/+$/, "");
+const url = `${scheme}://${host}${port ? `:${port}` : ""}${path}`;
 
 export const emipass = createClient<paths>({ baseUrl: url });
--- src/api/whip/client.ts
+++ src/api/whip/client.ts
@@ -5,8 +5,15 @@ import type { paths } from "./types";
 
 const scheme = process.env.EMIWEB__EMIPASS__WHIP__SCHEME || "http";
 const host = process.env.EMIWEB__EMIPASS__WHIP__HOST || "localhost";
-const port = process.env.EMIWEB__EMIPASS__WHIP__PORT || "11001";
-const path = process.env.EMIWEB__EMIPASS__WHIP__PATH || "";
-const url = `${scheme}://${host}:${port}${path}`;
+const port =
+  process.env.EMIWEB__EMIPASS__WHIP__PORT === undefined
+    ? 11001
+    : process.env.EMIWEB__EMIPASS__WHIP__PORT;
+const path = (process.env.EMIWEB__EMIPASS__WHIP__PATH || "")
+  // Ensure path starts with a slash
+  .replace(/^(?!\/)(.*)$/, "/$1")
+  // Remove trailing slashes
+  .replace(/\/+$/, "");
+const url = `${scheme}://${host}${port ? `:${port}` : ""}${path}`;
 
 export const whip = createClient<paths>({ baseUrl: url });
