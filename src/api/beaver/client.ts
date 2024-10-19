5415a4fd5fef2010ecf869acf55a90477b2ee737
Sebastian Pietras <me@spietras.dev>
2024-10-19 23:24:40 +0200

    Changed ports (#49)

--- src/api/beaver/client.ts
+++ src/api/beaver/client.ts
@@ -7,7 +7,7 @@ const scheme = process.env.POPPY__BEAVER__HTTP__SCHEME || "http";
 const host = process.env.POPPY__BEAVER__HTTP__HOST || "localhost";
 const port =
   process.env.POPPY__BEAVER__HTTP__PORT === undefined
-    ? 35000
+    ? 10500
     : process.env.POPPY__BEAVER__HTTP__PORT;
 const path = (process.env.POPPY__BEAVER__HTTP__PATH || "")
   // Ensure path starts with a slash
